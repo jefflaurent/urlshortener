9a491fc5756cce01dfb28cdbc8f76ae3d884cb98
eusebiusmarcel <emarcel@paystone.com>
2024-09-03 18:17:56 +0800

    Add URL shorten service

---
+++ src/main/java/com/example/urlshortener/service/URLShortenService.java
@@ -0,0 +1,38 @@
+package com.example.urlshortener.service;
+
+import java.util.Random;
+
+interface URLShortenServiceInterface {
+    String shortenURL(String longURL);
+}
+
+public class URLShortenService implements URLShortenServiceInterface {
+    DigitService digitService = new DigitService();
+    URLDAO urlDAO = new URLDAO();
+
+    public String shortenURL(String longURL) {
+        int length = digitService.queryStringLength();
+
+        for (int i = 0; i < 10; i++) {
+            String shortenedURL = generateRandomString(length);
+
+            boolean exists = urlDAO.queryShortenedURL(shortenedURL);
+            if (!exists) {
+                return shortenedURL;
+            } else if (i == 9) {
+                digitService.increaseStringLength();
+            }
+        }
+    }
+
+    String generateRandomString(int length) {
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder sb = new StringBuilder(length);
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+        return sb.toString();
+    }
+}
